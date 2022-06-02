2743bb9a2309ae8c334c3999de37f1d556f409d4
overjoyedbrass <riganadam@gmail.com>
2022-06-02 22:31:12 +0200

    fix in migrate script

--- server/database-scripts/migrate.sql
+++ server/database-scripts/migrate.sql
@@ -1,10 +1,10 @@
-DROP TABLE `dochadzka`.`absence_types`;
-CREATE TABLE `dochadzka`.`absence_types` (
+DROP TABLE IF EXISTS `absence_types`;
+CREATE TABLE `absence_types` (
   `type_id` INT NOT NULL AUTO_INCREMENT,
   `key` VARCHAR(45) NULL,
   `name` VARCHAR(45) NULL,
-  `confirmed` INT NULL DEFAULT 1,
-  PRIMARY KEY (`id`),
+  `default_confirmation` INT NULL DEFAULT 1,
+  PRIMARY KEY (`type_id`),
   UNIQUE INDEX `string_UNIQUE` (`key` ASC) VISIBLE)
 DEFAULT CHARACTER SET = utf8;
 
@@ -17,25 +17,25 @@ INSERT INTO `absence_types` VALUES
 (6,'ABSENCE_MATERNAL','Materská dovolenka',1),
 (7,'ABSENCE_PARENTAL','Rodičovská dovolenka',1);
 
-ALTER TABLE `dochadzka`.`absence` 
+ALTER TABLE `absence` 
 ADD CONSTRAINT `absence_types`
   FOREIGN KEY (`type`)
-  REFERENCES `dochadzka`.`absence_types` (`type_id`)
+  REFERENCES `absence_types` (`type_id`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
 
-DROP TABLE `dochadzka`.`perms`;
-CREATE TABLE `dochadzka`.`perms` (
+DROP TABLE IF EXISTS `perms`;
+CREATE TABLE `perms` (
   `role` INT NOT NULL,
   `perm` VARCHAR(45) NOT NULL,
    CONSTRAINT perm_key CHECK (perm IN ('user_managment', 'bypass_time', 'edit_budgets', 'edit_deadlines', 'edit_holidays', 'impersonate', 'manage_requests', 'export'))
  );
   
-ALTER TABLE `dochadzka`.`perms` 
-ADD UNIQUE INDEX `unique_row` (`role` ASC, `key` ASC);
+ALTER TABLE `perms` 
+ADD UNIQUE INDEX `unique_row` (`role` ASC, `perm` ASC);
 
 
-INSERT INTO `dochadzka`.`perms`
+INSERT INTO `perms`
 (`role`,
 `perm`)
 VALUES
@@ -47,31 +47,21 @@ VALUES
 (3, "export"),
 (3, "impersonate"),
 (3, "user_managment"),
-(4, "manage_requests")
+(4, "manage_requests");
 
-
-
-
-CREATE TABLE `dochadzka`.`tickets` (
+DROP TABLE IF EXISTS `tickets`;
+CREATE TABLE `tickets` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `user_id` INT NOT NULL,
-  `from_date` DATETIME NOT NULL,
-  `to_date` DATETIME NOT NULL,
-  `last_printed` DATETIME NULL DEFAULT NULL,
+  `from_date` DATE NOT NULL,
+  `to_date` DATE NOT NULL,
+  `last_printed` DATETIME NULL DEFAULT NULL DEFAULT NOW(),
   PRIMARY KEY (`id`),
   UNIQUE INDEX `TICKET_USER` (`user_id` ASC, `from_date` ASC) INVISIBLE,
   CONSTRAINT `user_id`
     FOREIGN KEY (`user_id`)
-    REFERENCES `dochadzka`.`users` (`id`)
+    REFERENCES `users` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 	
-ALTER TABLE `dochadzka`.`tickets` 
-CHANGE COLUMN `last_printed` `last_printed` DATETIME NULL DEFAULT NOW() ;
 
-ALTER TABLE `dochadzka`.`tickets` 
-CHANGE COLUMN `from_date` `from_date` DATE NOT NULL ,
-CHANGE COLUMN `to_date` `to_date` DATE NOT NULL ;
-
-ALTER TABLE `dochadzka`.`absence_types` 
-CHANGE COLUMN `confirmed` `default_confirmation` INT NULL DEFAULT '1' ;
